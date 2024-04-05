22b5d8decbf8f551a0fb1771eb57ddd8e3486cd8
samhou1988 <samhou1988@gmail.com>
2024-04-06 01:34:56 +0800

    feat: custom component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,29 +3,33 @@ import Image from 'next/image';
 
 import logoImg from '@/assets/logo.png';
 import styles from './Header.module.css';
+import HeaderBackground from '@/components/header-background';
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logo}>
-        <Image
-          src={logoImg.src}
-          width={250}
-          height={243}
-          alt="A plate with food on it" />
-        NextLevel Food
-      </Link>
+    <>
+      <HeaderBackground />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image
+            src={logoImg.src}
+            width={250}
+            height={243}
+            alt="A plate with food on it" />
+          NextLevel Food
+        </Link>
 
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
