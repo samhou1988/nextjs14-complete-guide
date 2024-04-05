f504ffbdf95b823819ed01f476720b9530186b23
samhou1988 <samhou1988@gmail.com>
2024-04-06 01:03:14 +0800

    feat: navigation page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
@@ -9,6 +11,7 @@ export default function Home() {
         alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p><Link href="/about">About Us</Link></p>
     </main>
   );
 }
