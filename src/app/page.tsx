b23b8a15d7fe73693e5f49d63d2582ca3eb55d9f
samhou1988 <samhou1988@gmail.com>
2024-04-06 01:09:26 +0800

    feat: dynamic route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Header from '@/components/header';
+
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/logo.png"
-        width={250}
-        height={243}
-        alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p><Link href="/about">About Us</Link></p>
     </main>
