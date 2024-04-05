d745758ac16e077001ab5e74f302b15dbe3fc828
samhou1988 <samhou1988@gmail.com>
2024-04-06 01:28:00 +0800

    feat: css module

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,31 @@
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
+
+import logoImg from '@/assets/logo.png';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <>
-      <Image
-        width={250}
-        height={243}
-        src="/logo.png"
-        alt="A server surrounded by magic sparkles."
-      />
-      <h1>Welcome to this NextJS Course!</h1>
-    </>
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        <Image
+          src={logoImg.src}
+          width={250}
+          height={243}
+          alt="A plate with food on it" />
+        NextLevel Food
+      </Link>
+
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
